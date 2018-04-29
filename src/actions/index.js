7cc43f0a845427c168d02833b20708bc27c86848
medwards1771 <meredith.edwards1771@gmail.com>
2018-04-29 09:20:21 -0400

    Use ReduxPromise; call weather API

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = "a7b76b76bed3a3ec8ffe16138b5e9309"
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
